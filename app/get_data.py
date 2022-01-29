d2025f08a543c430df55e58f58b86e6a67300829
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2022-01-29 16:58:00 +0100

    Imported data from google sheets

--- app/get_data.py
+++ app/get_data.py
@@ -1,1 +1,22 @@
-# This part of the program will go and retrieve the data from google drive and store it in the raw_data folder
+# This document is for getting the data, converting to a CSV file and storing it in 'data/raw'.
+
+""" Connect to google sheets """
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('../MDstatistics/app/credentials/creds.json', scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open('Master Duel - Deck stats').sheet1
+
+data = sheet.get_all_records()
+
+## Convert to excel
+import pandas as pd
+
+df = pd.DataFrame(data)
+
+df.to_excel('../MDstatistics/app/data/raw/raw.xlsx')
